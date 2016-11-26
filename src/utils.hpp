e66b871834f236219e05a3508a48f248d848ddef
Matheus Bittencourt <matheuspb2005@hotmail.com>
2016-11-26 18:51:44 -0200

    Separated declaration and implementation of utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -10,56 +10,14 @@
 #include <memory>
 #include <unistd.h>
 
-std::string read_file(std::string filename) {
-	std::ifstream ifs{filename};
-
-	if (!ifs) {
-		throw std::runtime_error("std::string read_file(std::string) : \
-Couldn't open file " + filename);
-	}
-
-	std::string content{std::istreambuf_iterator<char>(ifs),
-		std::istreambuf_iterator<char>()};
-	return content;
-}
+std::string read_file(std::string filename);
 
 // executes a linux command
-std::string exec(const std::string cmd) {
-	char buffer[128];
-	std::string result = "";
-	std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
+std::string exec(const std::string cmd);
 
-	if (!pipe) {
-		throw std::runtime_error("exec: popen() failed!");
-	}
-
-	while (!feof(pipe.get())) {
-		if (fgets(buffer, 128, pipe.get()) != NULL)
-			result += buffer;
-	}
-
-	return result;
-}
-
-std::vector<std::string> split(std::string str, const char* c) {
-	std::vector<std::string> output;
-
-	std::size_t begin = 0;
-	auto end = str.find_first_of(c);
-
-	while (begin < str.length()) {
-		output.push_back(str.substr(begin, end - begin));
-		begin = end + 1;
-		end = str.find_first_of(c, begin);
-	}
-
-	return output;
-}
+std::vector<std::string> split(std::string str, const char* c);
 
 template<typename T>
-void print_vector(std::vector<T> v) {
-	std::copy(v.begin(), v.end(),
-			std::ostream_iterator<T>(std::cout, ", "));
-}
+void print_vector(std::vector<T> v);
 
 #endif
